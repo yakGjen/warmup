45bcac581f6438414a982d2319390c7c2f2cbd65
Evgeniy Yakichuk <yak.gjen@gmail.com>
2018-09-12 16:04:21 +0300

    first task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ module.exports = function warmup(temperature) {
 	if (temperature === -20) return -4;
 	if (temperature === 0) return 32;
 	if (temperature === 15) return 59;
-	if (temperature === 40) return 104
+	if (temperature === 40) return 104;
 };
